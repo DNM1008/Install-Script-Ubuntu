29dd3cb153faf5cfd9ece0b183e2809af9fccb7d
DNM1008 <dungnguyen10082000@gmail.com>
2024-07-07 22:53:07 +1000

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 user=$(whoami)
 wd=$(pwd)
 # Installing stuff
@@ -6,6 +7,14 @@ wd=$(pwd)
 #
 #
 echo "Installing stuff"
+echo "Installing dependencies"
+sudo apt install git python3-pip
+
+echo "Python externally-managed environment"
+sudo rm /usr/lib/python3.12/EXTERNALLY-MANAGED
+
+
+
 echo "Stuff that doesn't use apt"
 echo "Antidot"
 cd ~/Downloads
@@ -15,4 +24,15 @@ chmod +x antidot
 echo "Moving antidot to a folder in PATH"
 mv antidot ~/.local/bin
 
+echo "Qtile and such"
 
+pip install qtile qtile-extras psutil asyncio pulsectl-asyncio
+
+echo "Desktop Entry for Qtile"
+sudo cp $wd/qtile.desktop /usr/share/xsessions/qtile.desktop
+
+echo "fastfetch"
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+
+echo "Apt stuff"
+sudo apt install fastfetch alacritty rofi xwallpaper thunar i3lock udiskie
